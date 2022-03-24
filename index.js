44c22d1a4fc88624b7a3ef5308d724b63c3175d5
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2022-03-24 10:55:40 +0800

    feat: modal component added

---
+++ index.js
@@ -0,0 +1,14 @@
+const modalClose = document.querySelector(".modal-close");
+const modal = document.querySelector('.modal');
+const modalShow = document.querySelector('.modal-show');
+
+
+const handleModalClose = () => {
+        modal.style.display = 'none'
+}
+const handleModalShow = () => {
+    modal.style.display = 'block'
+}
+
+modalClose.addEventListener('click' , handleModalClose)  
+modalShow.addEventListener('click', handleModalShow) 
